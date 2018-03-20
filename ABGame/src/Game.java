014a81a0b2eb085396e4b2b9739d69806adcf53c
tpppeggi <tpppeggi@gmail.com>
2018-03-20 16:16:38 +0800

    106/03/20

---
+++ ABGame/src/Game.java
@@ -0,0 +1,15 @@
+
+public class Game {
+
+	public static void main(String[] args) {
+		String secret = "8473";
+		String nums = "2819";
+		int length = secret.length();
+		for (int i = 0; i < length; i++) {
+			char c = secret.charAt(i);
+			System.out.println(c);
+		}
+
+	}
+
+}
